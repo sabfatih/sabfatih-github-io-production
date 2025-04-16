c9729dd73e509df6514f5f510e891f560b52321e
Fatih <alpha5808r@gmail.com>
2025-04-16 21:01:45 +0700

    Add border animation on button

--- src/App.jsx
+++ src/App.jsx
@@ -23,24 +23,26 @@ function App() {
       <div className="relative w-62 mx-auto mt-52 h-42">
         <div className="absolute z-10 w-62 h-42 bg-gradient-to-b from-[#1c1f2c] to-[#1a1c27] rounded-md px-7 py-4">
           <article className="text-white text-center font_poppins space-y-4">
-            <h1 className="text-2xl font-bold">Check out my portfolio</h1>
-            <a
-              href="https://sabihisma.vercel.app/"
-              className="py-2 cursor-pointer group hover:scale-105 active:scale-100 transition-all"
-            >
-              <button className="border text-xl rounded-md px-6 py-2 cursor-pointer group-hover:scale-105 group-active:scale-100 transition-all">
+            <h1 className="text-xl font-bold">
+              Go Check <br /> My Portfolio
+            </h1>
+
+            <button className="relative w-24 h-12 mx-auto group">
+              <a
+                href="https://sabihisma.vercel.app/"
+                className="absolute z-10 w-24 h-12 inset-0 bg-gradient-to-b from-[#1c1f2c] to-[#1a1c27] text-xl rounded-md cursor-pointer px-4 py-2.5 group-hover:scale-105 group-active:scale-100 transition-all"
+              >
                 Visit
-              </button>
-            </a>
+              </a>
+
+              <div className="absolute size-[102%] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 reverse_animated_bg_conic rounded-md group-hover:scale-105 group-active:scale-100 transition-all"></div>
+              <div className="absolute size-[102%] blur-sm top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 reverse_animated_bg_conic rounded-md group-hover:scale-105 group-active:scale-100 transition-all"></div>
+            </button>
           </article>
         </div>
 
-        <div
-          className={`absolute size-[105%] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 animated_bg_conic rounded-lg`}
-        ></div>
-        <div
-          className={`absolute size-[120%] blur-xl top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 animated_bg_conic rounded-lg`}
-        ></div>
+        <div className="absolute size-[105%] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 animated_bg_conic rounded-md"></div>
+        <div className="absolute size-[120%] blur-xl top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 animated_bg_conic rounded-md"></div>
       </div>
     </div>
   );
